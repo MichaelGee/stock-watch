8a15e2a4c218666fc22e73fa61ddd5c010075bd6
Michael <michaelgatesjr@gmail.com>
2020-07-03 21:40:47 +0100

    card added

--- src/components/card.js
+++ src/components/card.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Card, Col, Row } from "antd";
+import { Card } from "antd";
+import styled from "styled-components";
 
 const BasicCard = () => {
   const card = {
@@ -8,28 +9,37 @@ const BasicCard = () => {
     borderColor: "transparent",
     boxShadow:
       "0 1px 2px -2px rgba(0, 0, 0, 0.16), 0 3px 6px 0 rgba(0, 0, 0, 0.12), 0 5px 12px 4px rgba(0, 0, 0, 0.09)",
+    width: "80vw",
+    padding: "12px",
   };
 
+  const ContentContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    grid-gap: 0px;
+    @media (max-width: 1214px) {
+      grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    }
+    @media (max-width: 714px) {
+      grid-template-columns: repeat(auto-fit, minmax(130px, 1fr));
+    }
+  `;
+  const CardContent = styled.div`
+    padding: 1rem;
+  `;
   return (
     <div>
       <div className='site-card-wrapper'>
-        <Row gutter={16}>
-          <Col span={8}>
-            <Card style={card} bordered={false}>
-              Card content
-            </Card>
-          </Col>
-          <Col span={8}>
-            <Card style={card} bordered={false}>
-              Card content
-            </Card>
-          </Col>
-          <Col span={8}>
-            <Card style={card} bordered={false}>
-              Card content
-            </Card>
-          </Col>
-        </Row>
+        <Card style={card} bordered={false}>
+          <ContentContainer>
+            <CardContent>Card Content</CardContent>
+            <CardContent>Card Content</CardContent>
+            <CardContent>Card Content</CardContent>
+            <CardContent>Card Content</CardContent>
+            <CardContent>Card Content</CardContent>
+            <CardContent>Card Content</CardContent>
+          </ContentContainer>
+        </Card>
       </div>
     </div>
   );
