fa92f6e09bf7856f0efbdd3afbaedc62177b73ac
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2020-07-21 21:29:05 +0100

    Fix: apple price sorted

--- src/components/cards/apple.js
+++ src/components/cards/apple.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { Card } from "antd";
-import styled from "styled-components";
+import React from 'react';
+import { Card } from 'antd';
+import styled from 'styled-components';
 
 const CardWrapper = styled.div`
   display: flex;
@@ -11,6 +11,10 @@ const CardWrapper = styled.div`
     margin: 1rem;
   }
 `;
+// If you want the number at 3DPs, change the two "100s" to "1000s"
+const modifyPrice = (price) => {
+  return Math.round((price + Number.EPSILON) * 100) / 100;
+};
 
 const Title = styled.div`
   background: #e8ecef;
@@ -26,24 +30,24 @@ const Apple = ({ data: { price, open, close, high, low, volume } }) => {
   return (
     <div>
       <CardWrapper>
-        <Card className='card'>
+        <Card className="card">
           <Title>Apple</Title>
-          <Card.Grid className='card-grid' hoverable={false}>
-            Price: ${price}
+          <Card.Grid className="card-grid" hoverable={false}>
+            Price: ${modifyPrice(price)}
           </Card.Grid>
-          <Card.Grid className='card-grid' hoverable={false}>
+          <Card.Grid className="card-grid" hoverable={false}>
             Open: {open}
           </Card.Grid>
-          <Card.Grid className='card-grid' hoverable={false}>
+          <Card.Grid className="card-grid" hoverable={false}>
             Close: {close}
           </Card.Grid>
-          <Card.Grid className='card-grid' hoverable={false}>
+          <Card.Grid className="card-grid" hoverable={false}>
             High: {high}
           </Card.Grid>
-          <Card.Grid className='card-grid' hoverable={false}>
+          <Card.Grid className="card-grid" hoverable={false}>
             Low: {low}
           </Card.Grid>
-          <Card.Grid className='card-grid' hoverable={false}>
+          <Card.Grid className="card-grid" hoverable={false}>
             Volume: {volume}
           </Card.Grid>
         </Card>
